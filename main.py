ce61afb487f53b5b6a5e24691b385d1107b86734
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-30 16:25:37 -0700

    Lab 5-1 Camerino A.

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-def getinput():
-    # ******************************
-    # Make your Code
-    # ******************************
-
-
-def getsum(v1, v2):
-    # ******************************
-    # Make your Code
-    # ******************************
-
-
-def printval(v1, v2, total):
-    # ******************************
-    # Make your Code
-    # ******************************
-
-
 def main():
     userval1 = getinput()
     userval2 = getinput()
     total = getsum(userval1, userval2)
     printval(userval1, userval2, total)
 
+def getinput():
+    # ******************************
+    # Make your Code
+    # ******************************
+ num = int(input('Enter number'))
+ return num
+
+def getsum(v1, v2):
+    # ******************************
+    # Make your Code
+    # ******************************
+ total = v1+v2
+ return total
+
+def printval(v1, v2, total):
+    # ******************************
+    # Make your Code
+    # ******************************
+ print(v1,v2,total)
 
 if __name__ == '__main__':
     main()
